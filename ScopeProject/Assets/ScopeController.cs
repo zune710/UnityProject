b6c11a0288edf1314191c73e9c24450ff52b5ce4
zune710 <126778780+zune710@users.noreply.github.com>
2023-04-26 23:25:32 +0900

    0426 mesh

--- ScopeProject/Assets/ScopeController.cs
+++ ScopeProject/Assets/ScopeController.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class ScopeController : MonoBehaviour
 {
     // ** 0 ~ 180
-    [Range(0.0f, 180.0f)]
-    public float Angle = 45.0f;
+    [Range(0.0f, 360.0f)]
+    public float Angle = 90.0f;
 
     // ** 가상의 선의 개수
-    [Range(10, 360)]
+    [Range(10, 72)]
     public int Segments = 30;
 
     // ** 반지름
@@ -18,63 +18,99 @@ public class ScopeController : MonoBehaviour
 
     public List<Vector3> PointList = new List<Vector3>();
 
+    private MeshFilter meshFilter;
+    private Mesh mesh;
 
-    public List<GameObject> LeftPointList = new List<GameObject>();
-    public List<GameObject> RightPointList = new List<GameObject>();
+
+    private void Awake()
+    {
+        meshFilter = GetComponent<MeshFilter>();
+        mesh = new Mesh();
+    }
+
+    void Start()
+    {
+        Angle = 90.0f;
+        Segments = 36;
+        radius = 5.0f;
 
 #if UNITY_EDITOR
 
 #else
 
 #endif
-
-    void Start()
-    {
-        Angle = 45.0f;
-        Segments = 30;
-        radius = 5.0f;
-
-
-
-
-
-#if UNITY_EDITOR  // 에디터 모드에서만 사용
-
-#else
-
-#endif
-        //float angle = Angle / Segments;
-
-        //for (int i = 0; i < Segments + 1; ++i)
-        //{
-        //    GameObject Object = new GameObject("EditorGizmo");
-
-        //    MyGizmo gizmo = Object.AddComponent<MyGizmo>();
-
-        //    Object.transform.position = new Vector3(
-        //        Mathf.Sin(-(angle * i) * Mathf.Deg2Rad),
-        //        0.0f,
-        //        Mathf.Cos(-(angle * i) * Mathf.Deg2Rad)) * radius + transform.position;
-
-        //    PointList.Add(Object.transform.position);
-        //}
     }
 
     void Update()
     {
+        // ** 0.0 좌표를 포함해야 하기 때문에 Segments + 1의 값으로 초기화
+        int PointCount = Segments + 1;
+
+        // ** 버텍스의 개수만큼 생성
+        Vector3[] vertices = new Vector3[PointCount];
+
+        // ** 첫 번째 원소값은 0.0f (시작 지점)
+        vertices[0] = Vector3.zero;
+
+        // ** 만약 Angle이 180도라면 기준값으로부터 -90의 위치부터 시작해야 하므로
+        // ** (Angle * 0.5f)의 값을 기준값에서 빼준다.
+        // ** Angle = 180 이므로, (180 * 0.5f) = 90
+        // ** 기준값은 = transform.eulerAngles.y
+        // ** 결과적으로 transform.eulerAngls.y - 90 으로 볼 수 있다.
+        float deltaAngle = transform.eulerAngles.y - (Angle * 0.5f);
+
+        // ** int i = 1 부터 시작해야 한다.
+        for (int i = 1; i < vertices.Length; ++i)
+        {
+            // ** 부채꼴을 형성하는 계산
+            Vector3 point = new Vector3(
+                Mathf.Sin(deltaAngle * Mathf.Deg2Rad),
+                0.0f,
+                Mathf.Cos(deltaAngle * Mathf.Deg2Rad));
+
+            Ray ray = new Ray(transform.position, point.normalized);
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, radius))
+                vertices[i] = hit.point;
+            else
+                vertices[i] = point * radius;
+
+            deltaAngle += Angle / (Segments - 1);
+        }
+
+        /* [두르기]
+         * 하나의 면을 이룰 수 있는 최소 단위 = 폴리곤
+         * 하나의 폴리곤은 3개의 버텍스가 필요하고,
+         * 버텍스의 개수가 3개라면 그 세 점을 잇는 순서가 필요하다.
+         * 예를 들어 버텍스의 개수가 4개라면 세 점을 잇는 두르기 순서는 2개가 필요하고,
+         * 버텍스의 개수가 5개라면 3개의 삼각형이 만들어지기 때문에 두르기 순서도 3개가 필요하다.
+         * 
+         * 공식 = [(버텍스 개수 - 2) * 3]
+         */
+        int[] triangles = new int[(PointCount - 2) * 3];
+
+        // ** 순서를 삼각형마다 설정
+        for(int i = 0; i < PointCount - 2; ++i)
+        {
+            triangles[i * 3] = 0;
+            triangles[i * 3 + 1] = i + 1;
+            triangles[i * 3 + 2] = i + 2;
+        }
+
+        // ** 이전 삼각형의 위치가 포함되어 그려질 수 있기 때문에 항상 지워줘야 한다.
+        mesh.Clear();
+
+        // ** 실제 메쉬에 적용
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+
+        // ** 메쉬를 필터에 적용
+        meshFilter.mesh = mesh;
+
+
 #if UNITY_EDITOR
-
-        //print(transform.eulerAngles.y);
-
-        //float angle = 360 / Segments;
-        //for (int i = 0; i < Segments; ++i)
-        //{
-        //    PointList2[i].transform.position = new Vector3(
-        //        Mathf.Cos((angle * i) * Mathf.Deg2Rad),
-        //        Mathf.Sin((angle * i) * Mathf.Deg2Rad),
-        //        0.0f) * radius + transform.position;
-
-        //}
 
 #else
 
--- ScopeProject/Assets/ScopeEditor.cs
+++ ScopeProject/Assets/ScopeEditor.cs
@@ -6,70 +6,31 @@ using UnityEditor;
 public class ScopeEditor : Editor  // Editor: component 내부를 변경  /  EditorWindow: Editor를 edit
 {
     // 변경사항 생기면 유니티 환경에 즉시 적용
-    private void OnSceneGUI()
-    {
-        ScopeController targetComponent = (ScopeController)target;
+    //private void OnSceneGUI()
+    //{
+    //    ScopeController targetComponent = (ScopeController)target;
 
-        Handles.DrawWireArc(targetComponent.transform.position, Vector3.up, Vector3.forward, 360.0f, targetComponent.radius);
+    //    Handles.DrawWireArc(targetComponent.transform.position, Vector3.up, Vector3.forward, 360.0f, targetComponent.radius);
 
+    //    float Segments = targetComponent.Angle / targetComponent.Segments;
 
-        float Segments = targetComponent.Angle / targetComponent.Segments;
+    //    for (int i = 0; i < targetComponent.Segments + 1; ++i)
+    //    {
+    //        Vector3 angleLeftPoint = new Vector3(
+    //            Mathf.Sin(-(Segments * i) * Mathf.Deg2Rad),
+    //            0.0f,
+    //            Mathf.Cos(-(Segments * i) * Mathf.Deg2Rad));
 
-        for (int i = 0; i < targetComponent.Segments + 1; ++i)
-        {
-            Vector3 angleLeftPoint = new Vector3(
-                Mathf.Sin(-(Segments * i) * Mathf.Deg2Rad),
-                0.0f,
-                Mathf.Cos(-(Segments * i) * Mathf.Deg2Rad));
+    //        Vector3 angleRightPoint = new Vector3(
+    //            Mathf.Sin((Segments * i) * Mathf.Deg2Rad),
+    //            0.0f,
+    //            Mathf.Cos((Segments * i) * Mathf.Deg2Rad));
 
-            Vector3 angleRightPoint = new Vector3(
-                Mathf.Sin((Segments * i) * Mathf.Deg2Rad),
-                0.0f,
-                Mathf.Cos((Segments * i) * Mathf.Deg2Rad));
+    //        Handles.DrawLine(targetComponent.transform.position,
+    //            targetComponent.transform.position + angleLeftPoint * targetComponent.radius);
 
-            Handles.DrawLine(targetComponent.transform.position,
-                targetComponent.transform.position + angleLeftPoint * targetComponent.radius);
-
-            Handles.DrawLine(targetComponent.transform.position,
-                targetComponent.transform.position + angleRightPoint * targetComponent.radius);
-
-
-
-            targetComponent.LeftPointList[i].transform.position = targetComponent.transform.position + angleLeftPoint * targetComponent.radius;
-            targetComponent.RightPointList[i].transform.position = targetComponent.transform.position + angleRightPoint * targetComponent.radius;
-        }
-    }
-
-    private void OnEnable()
-    {
-        GameObject gizmoPrefab = Resources.Load("EditorGizmo") as GameObject;
-
-        ScopeController targetComponent = (ScopeController)target;
-        float Segments = targetComponent.Angle / targetComponent.Segments;
-
-        for (int i = 0; i < targetComponent.LeftPointList.Count; ++i)
-        {
-            DestroyImmediate(targetComponent.LeftPointList[i]);
-            DestroyImmediate(targetComponent.RightPointList[i]);
-        }
-
-        targetComponent.LeftPointList.Clear();
-        targetComponent.RightPointList.Clear();
-
-        for (int i = 0; i < targetComponent.Segments + 1; ++i)
-        {
-            targetComponent.LeftPointList.Add(Instantiate(gizmoPrefab));
-            targetComponent.RightPointList.Add(Instantiate(gizmoPrefab));
-
-            targetComponent.LeftPointList[i].transform.position = new Vector3(
-                    Mathf.Sin(-(Segments * i) * Mathf.Deg2Rad),
-                    0.0f,
-                    Mathf.Cos(-(Segments * i) * Mathf.Deg2Rad)) * targetComponent.radius + targetComponent.transform.position;
-
-            targetComponent.RightPointList[i].transform.position = new Vector3(
-                   Mathf.Sin((Segments * i) * Mathf.Deg2Rad),
-                   0.0f,
-                   Mathf.Cos((Segments * i) * Mathf.Deg2Rad)) * targetComponent.radius + targetComponent.transform.position;
-        }
-    }
+    //        Handles.DrawLine(targetComponent.transform.position,
+    //            targetComponent.transform.position + angleRightPoint * targetComponent.radius);
+    //    }
+    //}
 }
